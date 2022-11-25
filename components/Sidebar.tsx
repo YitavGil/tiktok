3576bc18d0c4c20b5b343b7c96f0d1bbbaa8666f
YitavGil <yitavga@yahoo.com>
2022-11-25 17:25:42 +0200

    Sidebar complete

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -5,10 +5,15 @@ import Link from 'next/link';
 import GoogleLogin from 'react-google-login';
 import { AiFillHome, AiOutlineMenu } from 'react-icons/ai';
 import { ImCancelCircle } from 'react-icons/im';
+import Discover from './Discover';
+import SuggestedAccounts from './SuggestedAccounts';
+import Footer from './Footer';
 
 
 const Sidebar = () => {
   const [showSidebar, setShowSidebar] = useState(true);
+
+  const userProfile = false;
 
   const normalLink = 'flex items-center gap-3 hover:bg-primary p-3 justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded'
   return (
@@ -30,6 +35,32 @@ const Sidebar = () => {
               </div>
             </Link>
           </div>
+          {!userProfile && (
+            <div className='px-2 py-4 hidden xl:block'>
+              <p className='text-gray-400'>Log in to like and comment on videos</p>
+              <div className='pr-4'>
+                <GoogleLogin
+                  clientId=''
+                  render={(renderProps) => (
+                    <button
+                    className='bg-white text-lg text-[#f51997] border-[1px] border-[#f51997] font-semibold px-6 py-3 rounded-md outline-none w-full mt-3 hover:text-white hover:bg-[#f51997] cursor-pointer'
+                      onClick={renderProps.onClick}
+                      disabled={renderProps.disabled}
+                    >
+                      Log in
+                    </button>
+                  )}
+                  onSuccess={() => {}}
+                  onFailure={() => {}}
+                  cookiePolicy={'single_host_origin'}
+                />
+              </div>
+            </div>
+          )}
+
+          <Discover />
+          <SuggestedAccounts />
+          <Footer />
         </div>
       )}
     </div>
