140567f09be0be144a7b422a8985aee698ee220c
YitavGil <yitavga@yahoo.com>
2022-11-26 09:46:09 +0200

    Footer complete

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
+
+const List = ({ footerList, mt }: { footerList: string[], mt: boolean }) => (
+  <div className={`flex flex-wrap gap-2 ${mt && "mt-5"}`}>
+    {footerList.map((item) => (
+      <p
+        key={item}
+        className="text-gray-400 text-sm hover:underline cursor-pointer"
+      >
+        {item}
+      </p>
+    ))}
+  </div>
+);
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className="mt-6 hidden xl:block">
+      <List footerList={footerList1} mt={false} />
+      <List footerList={footerList2} mt />
+      <List footerList={footerList3} mt />
+      <p className="text-gray-300 text-sm mt-5">2023 TikTok</p>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
