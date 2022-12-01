d242cce822256dc00d4ea011358a76b131274bc0
YitavGil <yitavga@yahoo.com>
2022-12-01 17:38:23 +0200

    Video section setup and types file

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,24 @@
 import type { NextPage } from "next";
 import axios from "axios";
 
+// interface IProps {
+//   videos: 
+// }
 
-const Home: NextPage = ()  => {
+
+const Home: NextPage = ({ videos })  => {
+  console.log(videos);
+  
   return <h1 className="text-3xl underline font-bold">Hello World</h1>;
 }
 
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`)
-
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`)
 
   return {
     props: {
+      videos: data
 
     }
   }
